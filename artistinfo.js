30e6753441fa3309c4782cc2c6848018f683cb12
Sam Kissee <samkissee@gmail.com>
2018-03-02 12:41:01 -0800

    updating artist data

--- artistinfo.js
+++ artistinfo.js
@@ -1,1 +1,42 @@
-fd
+export const artists = [
+  {
+    "name": "Longwalk Shortdock",
+    "bio": "Longwalkshortdock esentially started the first time Dave King heard gritty electronic music in early eighties videogames. Strongly influenced by these sounds and melodys, Dave started recording and looping segments as a child. He also got a taste for sampling and recording; taping segments of his piano practice to fool his parents into thinking he was practicing in the other room when he was really playing nintendo. Currently, a veteran of sound design and electronic music production for well over a decade, Longwalkshortdock's music has stepped into a genre of its own",
+    "genre": "Electro",
+    "location": "Canada",
+    "picture": Images.longwalk,
+    "link": "https://soundcloud.com/longwalkshortdock"
+  },
+  {
+    "name": "Alex Banks",
+    "bio": "Since releasing his debut album Illuminate in the summer of 2014, Buckinghamshire-born and Brighton-based producer Alex Banks has progressed from being an in-demand remixer and Solid Steel ‘Mix of the Year’-winning DJ to that and so much more today. From touring Europe with Moderat to DJing in Tokyo for Red Bull Music Academy’s EMAF, via countless live performances at continental festivals including Germany’s legendary Melt, he’s raised his own bar, his own way. And what comes now and next is just as forward thinking as the material he’s put out previously, unbounded by electronic music’s typically rigid genre barriers and capable of connecting with an audience on a cerebral as well as a physical level.",
+    "genre": "Techno",
+    "location": "UK",
+    "picture": Images.alexBanks,
+    "link": "https://soundcloud.com/alexbanksmusic"
+  },
+  {
+    "name": "Olivier Giacomotto",
+    "bio": "International DJ, charting producer, in-demand remixer, Beatport Music Awards nominee, Olivier Giacomotto is a proven integral player of the electronic music scene. He first got into production in 1999 working as producer assistant at Townhouse Studios in London, then went back to France with enough experience to start a career of music composer. His tireless need to evolve and grow as creative entity and forward thinking artist always forced this Paris based composer to constantly seek a new musical excitement. Therefore Olivier naturally crossed musical genres and trends always keeping an eye on the original definition of the underground.",
+    "genre": "Tech House",
+    "location": "Paris, France",
+    "picture": Images.olivier,
+    "link": "https://soundcloud.com/oliviergiacomotto"
+  },
+  {
+    "name": "Dirtwire",
+    "bio": "Dirtwire is David Satori of Beats Antique, Evan Fraser of Bolo and Mark Reveley of Jed and Lucia. Dirtwire sits on the front porch of Americana’s future, conjuring up a whirlwind of sound using traditional instrumentation, world percussion, soundscapes, and electronic beats. Comprised of David Satori (Beats Antique), Evan Fraser (Hamsa Lila; Bolo), and Mark Reveley (Jed and Lucia), each performance brings both band and audience to a mysterious crossroads of beats, blues, African, Asian, and South American sounds. The result is a rebirth of Americana and a post-millennial psychedelic journey to downhome goodness.",
+    "genre": "World Bass",
+    "location": "Oakland, CA",
+    "picture": Images.dirtwire,
+    "link": "https://soundcloud.com/dirtwire"
+  },
+  {
+    "name": "Five Alarm Funk",
+    "bio": "Dirtwire is David Satori of Beats Antique, Evan Fraser of Bolo and Mark Reveley of Jed and Lucia. Dirtwire sits on the front porch of Americana’s future, conjuring up a whirlwind of sound using traditional instrumentation, world percussion, soundscapes, and electronic beats. Comprised of David Satori (Beats Antique), Evan Fraser (Hamsa Lila; Bolo), and Mark Reveley (Jed and Lucia), each performance brings both band and audience to a mysterious crossroads of beats, blues, African, Asian, and South American sounds. The result is a rebirth of Americana and a post-millennial psychedelic journey to downhome goodness.",
+    "genre": "World Bass",
+    "location": "Oakland, CA",
+    "picture": Images.olivier,
+    "link": "https://soundcloud.com/dirtwire"
+  },
+]
